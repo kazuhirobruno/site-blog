f75757a467dc413101bf2bd43371cbc4df789503
kazuhirobruno <brunokazuhiro@gmail.com>
2025-08-27 22:10:14 -0300

    initial commit and tailwind adjustments

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen min-w-full bg-slate-500">
+      <h2 className="text-4xl text-green-400">Title</h2>
+    </div>
+  );
+}
